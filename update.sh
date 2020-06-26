673359fe1cfb78b5011fc852e9d561648ceae550
vantamm27 <vantamm27@gmail.com>
2020-06-26 16:56:36 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -10,13 +10,6 @@ cp node_exporter-$VERSION.linux-amd64/node_exporter /usr/local/bin/node_exporter
 
 exists=$(grep -c "^node_exporter:" /etc/passwd) && echo $exists
 
-if [ $exists -eq 0 ]; then
-    useradd -m -s /bin/bash node_exporter
-
-fi 
 rm -rf node_exporter-$VERSION.linux-amd64.tar.gz node_exporter-$VERSION.linux-amd64
-
-wget -O /etc/systemd/system/node_exporter.service   https://raw.githubusercontent.com/vantamm27/node_exporter/master/node_exporter.service
-
 
 systemctl start node_exporter
