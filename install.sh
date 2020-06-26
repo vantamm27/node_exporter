1f60c8b43d678466ab5778bf1aaff6400cc40c76
vantamm27 <vantamm27@gmail.com>
2020-06-26 15:58:16 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ wget  https://github.com/prometheus/node_exporter/releases/download/v$VERSION/no
 
 tar xvf node_exporter-$VERSION.linux-amd64.tar.gz
 
-cp node_exporter-$VERSION.linux-amd64/node_exporter /usr/local/bin
+cp node_exporter-$VERSION.linux-amd64/node_exporter /usr/local/bin/node_exporter
 
 exists=$(grep -c "^node_exporter:" /etc/passwd) && echo $exists
 
@@ -16,9 +16,11 @@ if [ $exists -eq 0 ]; then
 fi 
 
 
-sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
 
 rm -rf node_exporter-$VERSION.linux-amd64.tar.gz node_exporter-$VERSION.linux-amd64
+
+
+nano /etc/systemd/system/node_exporter.service
 
 wget -O /etc/systemd/system/node_exporter.service   https://raw.githubusercontent.com/vantamm27/node_exporter/master/node_exporter.service
 
@@ -33,9 +35,8 @@ mkdir -p /opt/iot/node_exporter
 echo '#!/bin/bash' > node_exporter.sh 
 echo "PUSHGATEWAY_SERVER=$ENDPOINT" >>  /opt/iot/node_exporter/node_exporter.sh
 echo "NODE_NAME=$HOSTNAME" >> /opt/iot/node_exporter/node_exporter.sh 
-echo "curl -s localhost:9101/metrics | curl --data-binary @- \$PUSHGATEWAY_SERVER/metrics/job/node-exporter/instance/\$NODE_NAME " >>  /opt/iot/node_exporter/node_exporter.sh 
+echo "curl -s localhost:9100/metrics | curl --data-binary @- \$PUSHGATEWAY_SERVER/metrics/job/node-exporter/instance/\$NODE_NAME " >>  /opt/iot/node_exporter/node_exporter.sh 
 
 chmod +x /opt/iot/node_exporter/node_exporter.sh
 
 crontab -l | { cat; echo "*/1 * * * * /opt/iot/node_exporter/node_exporter.sh"; } | crontab -
-
